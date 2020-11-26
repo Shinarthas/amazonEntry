122b32adec1d2d1b1f19bc36df1377c6bf97d59c
Shinarthas <tttaaabbb@gmail.com>
2020-11-26 12:46:18 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', 'Controller@index');
+Route::get('/amazon/callback', 'AmazonController@callback')->name('amazonCallback');
+Route::get('/amazon/{id}', 'AmazonController@show')->name('amazonLink');
+
